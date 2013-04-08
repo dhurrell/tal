b1686647cce6657808fa0195073ed5bf1112efaf
David Hurrell <david.hurrell@bbc.co.uk>
2013-04-08 17:22:54 +0100

    Clean up remote speech API. Remote endpoint now in config.

--- static/script/devices/speech/remoteapi.js
+++ static/script/devices/speech/remoteapi.js
@@ -6,7 +6,6 @@ require.def(
          'antie/audiosource'
     ],
     function(Class, Device, AudioSource) {
-        var remoteApiPrefix = "http://tts-api.com/tts.mp3?q=";
         var Speaker = Class.extend({
             /**
              * Initialise speech, passing in the device handle. 
@@ -14,9 +13,13 @@ require.def(
             init: function(device) {
                 this._player = device.createPlayer('speechPlayer', 'audio');
                 this._logger = device.getLogger();
-                this._logger.debug("Initialised remoteapi speech synth");
+                this._remoteApiPrefix = device.getConfig().voice && device.getConfig().voice.api || false;
                 
-                addEventListeners(this._player, this._logger);
+                if(!this._remoteApiPrefix) {
+                    throw new Error("No remote API specified in config.");
+                }
+                
+                this._logger.debug("Initialised remoteapi speech synth");
             },
             
             speak: function(text, onComplete) {
@@ -26,7 +29,7 @@ require.def(
                     onComplete();
                 };
                 
-                var sourceUrl = remoteApiPrefix + encodeURI(text);
+                var sourceUrl = this._remoteApiPrefix + text; // AudioSource calls encodeURI()
                 var audioSource = new AudioSource(sourceUrl, 'audio/mp4');
                 this._player.setSources([audioSource]);
                 
@@ -36,7 +39,6 @@ require.def(
                 
                 this._player.load();
                 this._player.play();
-                this._logger.debug("Excuse me I am talking!");
             },
             
             stop: function() {
@@ -47,28 +49,6 @@ require.def(
                 this._player.destroy();
             }
         });
-        
-        function addEventListeners(player, logger) {
-            logger.log('adding listeners on ' + player.id);
-            var events = ["loadstart", "progress", "suspend", "abort", "error", "emptied", "stalled", "play", "pause", "loadedmetadata", "loadeddata", "waiting", "playing", "canplay", "canplaythrough", "seeking", "timeupdate", "ended", "ratechange", "durationchange", "volumechange"];
-            for (var i = 0; i < events.length; i++) {
-                var e = events[i];
-                logger.log('adding ' + e + ' listener...');
-                player._mediaElement.addEventListener(e, function(e) {
-                    /*if(e.type === 'canplaythrough') {
-                        logger.log("ok I'm playing");
-                        player.yesiamloaded = true;
-                    }*/
-                    logger.log('event on ' + player.id + ': ' + e.type);
-                });
-            }
-            player.addEventListener("error", function(e) {
-               logger.log('error event on ' + player.id + '. Last? ' + e.last); 
-            });
-            /*player._mediaElement.addEventListener("play", function() {
-                debugger;
-            });*/
-        }
 
         Device.prototype.createSpeechSynth = function() {
             return new Speaker(this);
