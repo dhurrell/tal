4c5f11d80dd6aae20a21d08be98586766bc5edd7
David Hurrell <git@huz.org.uk>
2013-04-17 21:42:19 +0100

    Cleanup to chunker

--- static/script/devices/speech/chunker.js
+++ static/script/devices/speech/chunker.js
@@ -7,14 +7,22 @@ require.def(
         return Class.extend({
             /**
              * Use this class to split a long piece of speech into more manageable chunks.
-             * @param text String The entire chunk of text.
-             * @param Number Maximum length of each chunk, in characters.
+             * @param {Array|String} text The entire block of text - either an array of phrases or a single string.
+             * @param {Number} chunkLength Maximum length of each chunk, in characters.
              */
             init: function(text, chunkLength) {
                 if (typeof text === 'string') {
                     text = [text];
                 }
-                console.log('CHUNKER: ' + text.join('|'));
+                
+                // Remove empty elements from the array
+                for (var i = text.length; i >= 0; i--) {
+                    if (typeof text[i] !== 'string' || !text.trim()) {
+                        // Remove the element
+                        text.splice(i, 1);
+                    }
+                }
+                
                 this._text = text;
                 this._desiredLength = chunkLength || 100;
                 this._currentPosition = 0;
@@ -23,22 +31,19 @@ require.def(
             
             getNextChunk: function() {
                 var self = this;
-                var startPosition = this._currentPosition;
                 
                 if (this._currentPosition >= this._text[this._index].length) {
                     this._index++;
                     this._currentPosition = 0;
-                    startPosition = 0;
                 }
                 
+                var startPosition = this._currentPosition;
                 var text = this._text[this._index];
                 var nextBoundaryLength = findNextChunkLength();
                 
                 this._currentPosition += nextBoundaryLength;
                 
-                var test = text.substr(startPosition, nextBoundaryLength).trim();
-                console.log('NEXT:' + test);
-                return test;
+                return text.substr(startPosition, nextBoundaryLength).trim();
                 
                 // Helper function: Find the position of the next chunk boundary in the string.
                 function findNextChunkLength() {
