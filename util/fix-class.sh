e46d2a21a01f0d0a07aee988ca54814284c3308c
David Hurrell <git@huz.org.uk>
2015-05-15 07:08:45 +0100

    Tweak to the _super regexp, add usage example

--- util/fix-class.sh
+++ util/fix-class.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# find . -name \*.js -exec fix-class.sh {} \;
 
-perl -w -pi -e 'BEGIN{undef $/;} s/(\w+)(:\s*)function(?s)(.+?)this._super\s*\((.*?)\)/$1$2function $1$3$1.base.call(this, $4)/smg' /tmp/cehtml.js
-perl -w -pi -e 's/\.base\.call\(this,\s+\)/.base.call(this)/g' /tmp/cehtml.js
+perl -w -pi -e 'BEGIN{undef $/;} s/(\w+)(:\s*)function(?s)([^:]+?)this._super\s*\((.*?)\)/$1$2function $1$3$1.base.call(this, $4)/smg' $1
+perl -w -pi -e 's/\.base\.call\(this,\s+\)/.base.call(this)/g' $1
