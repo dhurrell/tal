685707841a74fec51665696cd4a1ac18562b9786
David Hurrell <david.hurrell@bbc.co.uk>
2015-06-01 18:22:05 +0100

    Try the new streamlined class.js

--- static/script/class.js
+++ static/script/class.js
@@ -11,7 +11,7 @@
 
 require.def('antie/class', function() {
   "use strict";
-  var initializing = false, fnTest = /var xyz/.test(function () { var xyz; }) ? /\b_super\b/ : /.*/;
+  var initializing = false;
 
 	/**
 	 * The base Class implementation (does nothing)
@@ -31,44 +31,28 @@ require.def('antie/class', function() {
 	 */
 	Class.extend = function(prop) {
         var _super = this.prototype;
-    
+
 		// Instantiate a base class (but only create the instance,
 		// don't run the init constructor)
 		initializing = true;
 		var proto = new this();
 		initializing = false;
-    
+
         // Copy the properties over onto the new prototype
         for (var name in prop) {
-          // Check if we're overwriting an existing function
-          proto[name] = typeof prop[name] === "function" && 
-            typeof _super[name] == "function" && fnTest.test(prop[name]) ?
-              (function(name, fn){
-                return function() {
-                  var tmp = this._super;
-    
-                  // Add a new ._super() method that is the same method
-                  // but on the super-class
-                  this._super = _super[name];
-    
-                  // The method only need to be bound temporarily, so we
-                  // remove it when we're done executing
-                  var ret = fn.apply(this, arguments);        
-                  this._super = tmp;
-    
-                  return ret;
-                };
-    		})(name, prop[name]) :
-            prop[name];
+            proto[name] = prop[name];
+            if (typeof prop[name] === 'function') {
+                proto[name].base = _super[name];
+            }
         }
-    
+
 		// The dummy class constructor
 		var newClass = function() {
 			// All construction is actually done in the init method
 			if ( !initializing && this.init )
 				this.init.apply(this, arguments);
 		};
-        
+
         // Populate our constructed prototype object
         newClass.prototype = proto;
     
@@ -77,7 +61,7 @@ require.def('antie/class', function() {
     
         // And make this class extendable
         newClass.extend = Class.extend;
-    
+
         return newClass;
       };
 
